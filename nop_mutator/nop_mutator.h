54bc4911f6c708f434d9a17bc023b9443edc3000
Ben Lipton <ben@grimm-co.com>
2018-04-11 14:45:07 -0400

    Update for fuzzer initial source release

--- nop_mutator/nop_mutator.h
+++ nop_mutator/nop_mutator.h
@@ -1,31 +1,30 @@
 #pragma once
 
 #include <global_types.h>
+#include "mutators.h"
 
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the NOP_MUTATOR_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// NOP_MUTATOR_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+#ifdef _WIN32
 #ifdef NOP_MUTATOR_EXPORTS
-#define NOP_MUTATOR_API extern "C" __declspec(dllexport)
+#define NOP_MUTATOR_API __declspec(dllexport)
 #else
 #define NOP_MUTATOR_API __declspec(dllimport)
 #endif
-
-#include "mutators.h"
+#else //_WIN32
+#define NOP_MUTATOR_API
+#endif
 
 #define MUTATOR_NAME "nop"
 
 NOP_MUTATOR_API void * FUNCNAME(create)(char * options, char * state, char * input, size_t input_length);
 NOP_MUTATOR_API void FUNCNAME(cleanup)(void * mutator_state);
 NOP_MUTATOR_API int FUNCNAME(mutate)(void * mutator_state, char * buffer, size_t buffer_length);
+NOP_MUTATOR_API int FUNCNAME(mutate_extended)(void * mutator_state, char * buffer, size_t buffer_length, uint64_t flags);
 NOP_MUTATOR_API char * FUNCNAME(get_state)(void * mutator_state);
-NOP_MUTATOR_API void FUNCNAME(free_state)(char * state);
+NOP_MUTATOR_API void FUNCNAME(free_state)(char * mutator_state);
 NOP_MUTATOR_API int FUNCNAME(set_state)(void * mutator_state, char * state);
 NOP_MUTATOR_API int FUNCNAME(get_current_iteration)(void * mutator_state);
-NOP_MUTATOR_API int FUNCNAME(get_total_iteration_count)(void * mutator_state);
+#define nop_get_total_iteration_count return_unknown_or_infinite_total_iterations
+NOP_MUTATOR_API void FUNCNAME(get_input_info)(void * mutator_state, int * num_inputs, size_t **input_sizes);
 NOP_MUTATOR_API int FUNCNAME(set_input)(void * mutator_state, char * new_input, size_t input_length);
 NOP_MUTATOR_API int FUNCNAME(help)(char ** help_str);
 
